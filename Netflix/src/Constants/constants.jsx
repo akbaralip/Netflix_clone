a4ead44da5b54f9b28376fff49e61a28ffd6fe27
Akbar Ali <129048212+akbaralip@users.noreply.github.com>
2023-09-26 16:29:47 +0530

    Update Some Changes

--- Netflix/src/Constants/constants.jsx
+++ Netflix/src/Constants/constants.jsx
@@ -1,5 +1,5 @@
 export const baseUrl = 'https://api.themoviedb.org/3';
 
-export const API_KEY = 'e7dfda65eaf8e2c38f925376fdded936';
+export const API_KEY = 'your API_KEY please Add here';
 
 export const imageUrl = 'https://image.tmdb.org/t/p/original'
